8c855a6dc2082b38b15ce09eeb07060655a54392
Louis Popi <theh2o64@gmail.com>
2017-08-25 08:05:13 +0200

    Revert "tomato: libshims: Create shim for protobuf"
    This reverts commit cab3afbc4a828b97ec4726a1897764f29fe3c7a2.

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -13,7 +13,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
     $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml \
     $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml
-
-# Shim
-PRODUCT_PACKAGE += \
-    libshim_protobuf
