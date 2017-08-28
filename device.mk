45165ad3fcf99079cb1afc519002d16f9ace1f9f
Louis Popi <theh2o64@gmail.com>
2017-08-28 13:31:49 +0200

    Revert "tomato: Set default density"
    This reverts commit 8df58106a03e335d2bf399c47994979285785027.

--- device.mk
+++ device.mk
@@ -23,7 +23,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 #    $(LOCAL_PATH)/overlay-lineage
 
 # Due to multi-density builds, these are set by init
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model ro.sf.lcd_density
 
 # Include package config fragments
 include $(LOCAL_PATH)/product/*.mk
