d2d2fca95217f11a8db0e63af0e1cb7636c0ef07
ishantvivek <ishantvivek1311@gmail.com>
2017-08-29 12:11:44 +0000

    tomato: Build prima inline in the kernel (1/2)

--- board/qcom-wlan.mk
+++ board/qcom-wlan.mk
@@ -2,7 +2,3 @@
 # CONFIG_EAP_PROXY := qmi
 # CONFIG_EAP_PROXY_DUAL_SIM := true
 TARGET_PROVIDES_WCNSS_QMI := true
-
-# The uncompressed arm64 is too large, split wifi for now
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
-WIFI_DRIVER_MODULE_NAME := "wlan"
